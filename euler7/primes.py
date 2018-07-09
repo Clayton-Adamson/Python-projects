849f50bbd9d22e829c75d4729f328f084fb79d68
Windmill-2012 <40898857+Windmill-2012@users.noreply.github.com>
2018-07-08 19:44:33 -0600

    primes.py

---
+++ euler7/primes.py
@@ -0,0 +1,18 @@
+# PE 7
+# What is the 10001 prime number
+
+primes = [2,3,5,7,11,13]
+
+for x in range(14,105000):
+    for y in range(int(math.sqrt(x)+1),1,-1):
+        if(x % y == 0):
+            x=x+1
+            break
+
+        elif((x % y != 0) and (y==2)):
+            primes.append(x)
+            x=x+1
+            continue
+
+
+print(str(primes[10000]) + " is the 10001 prime")
