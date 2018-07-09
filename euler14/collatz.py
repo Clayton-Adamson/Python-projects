a868dd086f0cc706087441fb9b554d24a663cb5b
Windmill-2012 <40898857+Windmill-2012@users.noreply.github.com>
2018-07-08 21:43:25 -0600

    collatz.py

---
+++ euler14/collatz.py
@@ -0,0 +1,29 @@
+# PE 14
+# Collatz sequences (find longest sequence)
+# if x is even, divide x by 2
+# if x is odd, return 3x+1
+# repeat until x=1
+
+answer = 0
+
+def seq(x):
+
+    collatz = [x]
+
+    while(True):
+        if(x==1):
+            return len(collatz)
+
+        elif(x % 2 == 0):
+            x=x/2
+            collatz.append(x)
+
+        else:
+            x=(3*x)+1
+            collatz.append(x)
+
+# I had to find the longest sequence in this range
+for x in range(1,1000000):
+    if(seq(x)>answer):
+        answer = seq(x)
+        print(str(x) + " has a chain of " + str(answer))
