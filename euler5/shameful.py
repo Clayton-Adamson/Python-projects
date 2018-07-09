ecdd2d86fe7f365fc08672d52a66752dc82e30a6
Windmill-2012 <40898857+Windmill-2012@users.noreply.github.com>
2018-07-08 22:04:39 -0600

    shameful.py

---
+++ euler5/shameful.py
@@ -0,0 +1,7 @@
+# PE 5
+# smallest positive number evenly divisible by all numbers 1 to 20
+# figured this out manually first, hardcoding was faster than figuring it out 
+# in a non-shameful way
+
+ans = 1*2*3*5*7*11*13*17*19*2*2*3*2
+print(ans) 
