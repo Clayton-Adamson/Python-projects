8cdf1bfb3b3884dbb34fd9f6689bdaf54ed12b68
Windmill-2012 <40898857+Windmill-2012@users.noreply.github.com>
2018-07-08 21:19:03 -0600

    triplets.py

---
+++ euler9/triplets.py
@@ -0,0 +1,16 @@
+# PE 9
+# pythagorean triplet, find product abc
+# when a+b+c = 1000
+
+for a in range(1,1000):
+    for b in range(2,1001):
+        if(a>b):
+            continue
+        else:
+            hyp = a**2 + b**2
+
+            if(a + b + pow(hyp,0.5) >= 1001):
+                continue
+            elif(a + b + pow(hyp,0.5) == 1000):
+                print(str(a + b + pow(hyp,0.5)) + " " + str(a) + " " + str(b))
+                print(a*b*pow(hyp,0.5))
