330eb15ea7a6660f927252e87dff8c1dacef2d1d
Windmill-2012 <40898857+Windmill-2012@users.noreply.github.com>
2018-07-10 22:10:05 -0600

    Update palindrome.py

--- euler4/palindrome.py
+++ euler4/palindrome.py
@@ -1,6 +1,32 @@
 # PE 4
 # Find largest palindrome from the product of 3 digit numbers
 
+
+# new way
+
+max_pal = 0
+
+def pal(num):
+    for x in range(0,3):
+        if(str(num)[x] != str(num)[5-x]):
+            return 0
+        elif(x==2):
+            return num
+        else:
+            continue
+
+
+for x in range(100,1000):
+    for y in range(100,1000):
+        num = x*y
+        if(len(str(num)) == 6 and pal(num) > max_pal ):
+            max_pal = num
+
+print(max_pal)
+
+
+
+'''
 master_list = []
 
 def pal(a,b):
@@ -36,3 +62,4 @@ for x in range(100,999):
 
 # I wish I made it so that i didn't need the master list outside the method, but w/e
 print(max(master_list))
+'''
